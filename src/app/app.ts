dcb00674649d0f42676d479e93f1f824a5fae8f2
Sivamurthy20 <sivamurthy2222gmail.com>
2025-07-19 16:50:09 +0530

    Initial Commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,28 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FormsModule, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('binding_data');
+  
+  //Interpolation
+  username:string = 'User';
+
+  //Two-way binding
+  userInput: string = '';
+
+  //Event binding example
+  sayHello() {
+    alert(`Hello, ${this.username!}`);
+  }
+
+  onUsernameInput(event: Event) :void {
+    this.username = (event.target as HTMLInputElement).value;
+  }
+  
 }
